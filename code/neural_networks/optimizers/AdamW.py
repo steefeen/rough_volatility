bab4d97e6cc083e07f940a53cf621edb0fe5e679
Stefan Englmeier <stefanenglmeier@gmail.com>
2021-02-20 14:54:21 +0100

    add optimizers AdamW and QHAdam

---
+++ code/neural_networks/optimizers/AdamW.py
@@ -0,0 +1,55 @@
+from keras.optimizers import Optimizer
+from keras.legacy import interfaces
+from keras import backend as K
+
+
+class AdamW(Optimizer):
+    def __init__(self, learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=0.0000001, w=0.0001, name="AdamW",
+                 **kwargs):
+
+        super(AdamW, self).__init__(**kwargs)
+        with K.name_scope(self.__class__.__name__):
+            self.iterations = K.variable(0, dtype='int64', name='iterations')
+            self.lr = K.variable(learning_rate, name='lr')
+            self.beta_1 = K.variable(beta_1, name='beta_1')
+            self.beta_2 = K.variable(beta_2, name='beta_2')
+            self.w = K.variable(w, name='w')
+            if epsilon is None:
+                epsilon = K.epsilon()
+            self.epsilon = epsilon
+
+    @interfaces.legacy_get_updates_support
+    def get_updates(self, loss, params):
+        grads = self.get_gradients(loss, params)
+        self.updates = [K.update_add(self.iterations, 1)]
+
+        lr = self.lr
+
+        ms = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
+        vs = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
+
+        for p, g, m, v in zip(params, grads, ms, vs):
+            m_t = (self.beta_1 * m) + (1. - self.beta_1) * g
+            v_t = (self.beta_2 * v) + (1. - self.beta_2) * K.square(g)
+
+            p_t = p - lr * (1 / (K.sqrt(v_t + self.epsilon)) * m_t + self.w * p)
+
+            self.updates.append(K.update(m, m_t))
+            self.updates.append(K.update(v, v_t))
+            new_p = p_t
+
+            # Apply constraints.
+            if getattr(p, 'constraint', None) is not None:
+                raise NotImplementedError
+                new_p = p.constraint(new_p)
+
+            self.updates.append(K.update(p, new_p))
+        return self.updates
+
+    def get_config(self):
+        config = {'learning_rate': float(K.get_value(self.lr)),
+                  'beta_1': float(K.get_value(self.beta_1)),
+                  'beta_2': float(K.get_value(self.beta_2)),
+                  'w': float(K.get_value(self.w))}
+        base_config = super(AdamW, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
